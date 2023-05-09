2625ff87115854c3a04c1b66279da5b131f6c89e
Robinson Berthet <berthet.robinson@gmail.com>
2023-05-09 22:49:58 +0200

    ajout post

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -31,7 +31,7 @@
           <div class="subhead">Contactez Moi</div>
           <div class="divider"></div>
 
-          <form name="contact" netlify>
+          <form name="contact" method="POST" netlify>
             <div class="py-2">
               <input type="text" class="form-control" placeholder="Full name" />
             </div>
