06633bdb17a9ddf17726b7bbbc41aa0954e53d66
Robinson Berthet <berthet.robinson@gmail.com>
2023-05-09 19:55:57 +0200

    test envoi form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -31,7 +31,7 @@
           <div class="subhead">Contactez Moi</div>
           <div class="divider"></div>
 
-          <form name="contact" method="POST" data-netlify="true">
+          <form name="contact" netlify netlify-honeypot="bot-field" hidden>
             <div class="py-2">
               <input type="text" class="form-control" placeholder="Full name" />
             </div>
@@ -69,28 +69,6 @@ export default {
     Footer: Footer,
   },
 };
-
-
-  const handleSubmit = (event) => {
-  event.preventDefault();
-  const myForm = event.target;
-  const formData = new FormData(myForm);
-  
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error));
-  };
-
-  document
-    .querySelector("form")
-    .addEventListener("submit", handleSubmit);
-
-
-
 </script>
 
 <style></style>
