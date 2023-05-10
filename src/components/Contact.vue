1a9cf4c2d9d144441324a728b7b4c134eb50a369
Robinson <berthet.robinson@gmail.com>
2023-05-10 13:49:03 +0200

    test redirection index

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -31,7 +31,7 @@
           <div class="subhead">Contactez Moi</div>
           <div class="divider"></div>
 
-          <form name="contact" method="POST" data-netlify="true">            
+          <form name="contact" action="index.html" method="POST" data-netlify="true">            
             <div class="py-2">
               <input type="text" class="form-control" placeholder="Votre Nom" name="name"/>
             </div>
