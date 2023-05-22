c2eba9104b6532576170386e78e378a89f62dbe8
Robinson Berthet <berthet.robinson@gmail.com>
2023-05-22 13:36:56 +0200

    test detail proj AP3

--- src/components/projets/AP3.vue
+++ src/components/projets/AP3.vue
@@ -1,4 +1,30 @@
 <template>
-    <p>AP3
-    </p>
+    <div class="widget-box">
+        <h3>AP3 Hackat'Innov</h3>
+        <p>
+            Ce projet est une évolution de l'application web Hackat'Innov. 
+            Cette application à pour but l'inscription à des hackathons(concours de code) pour différentes équipes.
+        </p>
+        <br>
+        <br>
+        <h3>Les Évolutions demandées étaient:</h3>
+        <p>
+            L'amélioration du profil des équipes (gestion des membres, désinscription, hackathons suivis) sur le web Connexion sécurisée aux données;
+            Finalisation de la gestion des hackathons;
+            Gestion des équipes et des membres par les administrateurs;
+            Gestion de documents statistiques en aide aux administrateurs;
+        </p>
+        <br>
+        <div class="flex">
+            <img class="imgPart" src="..\..\..\public\images\img_participants.png"/>
+            <p>Consultation des membres de chaque équipe quand on est connecté en tant qu’équipe</p>
+        </div>
+        <br>
+        <hr>
+        <br>
+        <div class="flex">
+            <img class="imgPart" src="..\..\..\public\images\imgCGU.png"/>
+            <p>Page à Propos avec des CGU basiques</p>
+        </div>
+    </div>
 </template>
